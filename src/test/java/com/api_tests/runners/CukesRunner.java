a9666309e32319aa4fd28e2c69a79eba31286f94
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-05-05 15:58:15 +0100

    All utilities and dependencies are ready

---
+++ src/test/java/com/api_tests/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.api_tests.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                  "html:target/default-html-reports"},
+        features = "src/test/resources/features/",
+        glue = "com/bbc_api/step_definitions/",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
